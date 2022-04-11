0314e196212ccd2b42a28d8e44e489be04f1a6c3
Nairo Elsner <46656582+nairoelsner@users.noreply.github.com>
2022-04-11 02:46:29 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const userSocialMediaLinks = {
+  github: 'nairoelsner',
+  youtube: 'madnesspiano',
+  facebook: 'nairo.elsner',
+  instagram: 'n_elsner',
+  twitter: 'nairo_elsner'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${userSocialMediaLinks[social]}`
+  }
+}
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${userSocialMediaLinks.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      pageTittle.textContent = data.name
+      userGitHubAvatar.src = data.avatar_url
+      userName.textContent = data.name
+      userGitHubLogin.textContent = data.login
+      userBio.textContent = data.bio
+      userGitHubLink.href = data.html_url
+    })
+}
+
+getGitHubProfileInfos()
